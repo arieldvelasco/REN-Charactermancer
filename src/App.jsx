22f0c3f45c088bd0fe3b9e198517af35cfd0953c
arieldvelasco <arield.velasco@gmail.com>
2023-11-26 15:01:28 -0300

    conversión a proyecto REACT

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Regnum ex Nihilo</h1>
+      <h3>Este es un editor de personajes para el sistema de Regnum ex Nihilo (ahora hecho con REACT)</h3>
+      <section>
+          <div>
+              <span>Nombre: </span><input type="text" name="" id="" />
+          </div>
+      </section>
+    </>
+  )
+}
+
+export default App
